69acc5d4980862a74152ed8e374199e84d7ea5c5
JulieValdez <juliegragg0605@gmail.com>
2020-02-10 13:46:25 -0800

    html done

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+// variable 
+
+
+
+
+
+
+
+
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBzQx62DI4dlCsh3ui0IunZm5mLMzuE9sQ",
+  authDomain: "first-firebase-project-8ff4d.firebaseapp.com",
+  databaseURL: "https://first-firebase-project-8ff4d.firebaseio.com",
+  projectId: "first-firebase-project-8ff4d",
+  storageBucket: "first-firebase-project-8ff4d.appspot.com",
+  messagingSenderId: "419297304139",
+  appId: "1:419297304139:web:51e68d7f8320580a2877ce"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
